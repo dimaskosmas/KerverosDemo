ea5e089f0b72cd4285ce72e33a48d6b9bf4d5329
Κοσμάς Δήμας <dimaskosmas@outlook.com.gr>
2017-11-14 13:17:28 +0200

    UI tutorial integration and simple delegate control button

--- KerverosDemo.UI/MainWindow.xaml.cs
+++ KerverosDemo.UI/MainWindow.xaml.cs
@@ -1,51 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace KerverosDemo.UI
+{ 
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            KerverosDemo.UI.KerverosDemoDataSet kerverosDemoDataSet = ((KerverosDemo.UI.KerverosDemoDataSet)(this.FindResource("kerverosDemoDataSet")));
-            // Load data into the table Customers. You can modify this code as needed.
-            KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter kerverosDemoDataSetCustomersTableAdapter = new KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter();
-            kerverosDemoDataSetCustomersTableAdapter.Fill(kerverosDemoDataSet.Customers);
-            System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
-            customersViewSource.View.MoveCurrentToFirst();
-            KerverosDemo.UI.KerverosDemoDataSet kerverosDemoDataSet = ((KerverosDemo.UI.KerverosDemoDataSet)(this.FindResource("kerverosDemoDataSet")));
-            // Load data into the table Customers. You can modify this code as needed.
-            KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter kerverosDemoDataSetCustomersTableAdapter = new KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter();
-            kerverosDemoDataSetCustomersTableAdapter.Fill(kerverosDemoDataSet.Customers);
-            System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
-            customersViewSource.View.MoveCurrentToFirst();
-            KerverosDemo.UI.KerverosDemoDataSet kerverosDemoDataSet = ((KerverosDemo.UI.KerverosDemoDataSet)(this.FindResource("kerverosDemoDataSet")));
-            // Load data into the table Customers. You can modify this code as needed.
-            KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter kerverosDemoDataSetCustomersTableAdapter = new KerverosDemo.UI.KerverosDemoDataSetTableAdapters.CustomersTableAdapter();
-            kerverosDemoDataSetCustomersTableAdapter.Fill(kerverosDemoDataSet.Customers);
-            System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
-            customersViewSource.View.MoveCurrentToFirst();
-        }
+        InitializeComponent();
     }
+
+    private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        ViewModel.StudentViewModel studentViewModelObject = new ViewModel.StudentViewModel();
+
+        studentViewModelObject.LoadStudents();
+
+        StudentViewControl.DataContext = studentViewModelObject;
+    }
+}
 }
